ed71bbfaa2e37fefc6514f0d3ad39033a4e715a5
Alberto Bisanti <bisanti.alberto98@gmail.com>
2022-10-03 23:33:47 +0200

    Added CRUD functionalities and minor bug fixes

--- products.php
+++ products.php
@@ -10,7 +10,7 @@ $conn = connect_db();
 
 switch($action){
     case 'getProducts':
-        $sql = "SELECT * FROM products";
+        $sql = "SELECT p.*,c.name as category_name FROM products p INNER JOIN categories c on category_id = c.id";
         $result = mysqli_query($conn,$sql);
         if(mysqli_num_rows($result) > 0) {
             $response['status'] = 'OK';
@@ -22,6 +22,20 @@ switch($action){
             $response['data']['error'] = "Non sono stati trovati prodotti. Prova ad inserirne uno!";
         }
         break;
+    case 'getSingleProduct':
+        if(!empty($_POST['id'])) {
+            $sql = "SELECT * FROM products where id = ?";
+            $statement = mysqli_prepare($conn,$sql);
+            mysqli_stmt_bind_param($statement,'i',$_POST['id']);
+            $rs = mysqli_stmt_execute($statement);
+            $rsArray = mysqli_fetch_assoc(mysqli_stmt_get_result($statement));
+            $response['status'] = !empty($rs) ? 'OK' : 'KO';
+            $response['data'] = $rsArray;
+        } else {
+            $response['status'] = 'KO';
+            $response['data']['error'] = "Non è stato fornito l'ID per completare l'operazione";
+        }
+        break;
     case 'addProduct':
         if(!empty($_POST['code']) && !empty($_POST['name'])){
             $sql = "INSERT INTO products(code, name, price, qta, image_name) values(?,?,?,?,?)";
@@ -29,6 +43,8 @@ switch($action){
             mysqli_stmt_bind_param($statement,'ssiis',$_POST['code'], $_POST['name'], $_POST['price'], $_POST['qta'], $_POST['image_name']);
             $rs = mysqli_stmt_execute($statement);
             $response['status'] = $rs ? 'OK' : 'KO';
+            $id = $conn->insert_id;
+            $response['data']['id'] = $id;
         } else {
             $response['status'] = 'KO';
             $response['data']['error'] = "Non è stato fornito un codice o un nome per il prodotto";
@@ -39,7 +55,7 @@ switch($action){
         if(!empty($_POST['id'])) {
             $sql = "UPDATE products SET code = ?, name = ?, price = ?, qta = ? where id = ?";
             $statement = mysqli_prepare($conn,$sql);
-            mysqli_stmt_bind_param($statement,'si',$_POST['name'],$_GET['id']);
+            mysqli_stmt_bind_param($statement,'ssiii',$_POST['code'],$_POST['name'],$_POST['price'],$_POST['qta'],$_POST['id']);
             $rs = mysqli_stmt_execute($statement);
             $response['status'] = $rs ? 'OK' : 'KO';
         } else {
@@ -50,9 +66,9 @@ switch($action){
 
     case 'deleteProduct':
         if(!empty($_POST['id'])) {
-            $sql = "DELETE FROM categories where id = ?";
+            $sql = "DELETE FROM products where id = ?";
             $statement = mysqli_prepare($conn,$sql);
-            mysqli_stmt_bind_param($statement,'i',$_GET['id']);
+            mysqli_stmt_bind_param($statement,'i',$_POST['id']);
             $rs = mysqli_stmt_execute($statement);
             $response['status'] = $rs ? 'OK' : 'KO';
         } else {
